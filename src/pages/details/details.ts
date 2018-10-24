6ea0b727977d547c9a54505117822cf9485b6310
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-10-24 09:07:54 +0200

    f

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform, LoadingController, ViewController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, LoadingController, ViewController } from 'ionic-angular';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import * as $ from "jquery";
@@ -14,133 +14,85 @@ import { FileTransfer } from '@ionic-native/file-transfer';
  * Ionic pages and navigation.
  */
 
-
 @Component({
-  selector: 'page-details',
-  templateUrl: 'details.html',
+	selector: 'page-details',
+	templateUrl: 'details.html',
 })
 export class DetailsPage {
-link: string;
-    title: string;
-    text: any;
-    date: string;
-   image:any;
-    cat: string;
+	link: string;
+	title: string;
+	text: any;
+	date: string;
+	image: any;
+	cat: string;
 	trustedPostUrl: SafeResourceUrl;
 	postsLoading: any;
 	setHeight: any;
 	posts: Array<any> = [];
 
+	constructor(public navCtrl: NavController,
+		public navParams: NavParams,
+		private sanitizer: DomSanitizer,
+		private socialSharing: SocialSharing,
+		public loadingCtrl: LoadingController,
+		private ga: GoogleAnalytics,
+		public viewCtrl: ViewController,
+		public http: Http,
+		private file: File,
+		private transfer: FileTransfer,
+		public plt: Platform
 
-  constructor( public navCtrl: NavController, 
-			   public navParams: NavParams, 
-			 	private sanitizer: DomSanitizer,
-				private socialSharing: SocialSharing,
-				public loadingCtrl: LoadingController,
-				private ga: GoogleAnalytics,
-				public viewCtrl: ViewController,
-				public http: Http,
-				private file: File,
-				private transfer: FileTransfer,
-				public plt: Platform,
+	) {
+		this.title = navParams.get('title');
+		this.link = navParams.get('link');
+		this.trustedPostUrl = this.sanitizer.bypassSecurityTrustResourceUrl('./assets/html/test.html');
+	}
 
-) {
-	  
-		 this.title = navParams.get('title');
-		 this.link = navParams.get('link');
+	/*
+	private downloadAndSavePicture(pictureUrl) {
+			this.http.get(pictureUrl, {responseType: 'blob'})
+			.subscribe((imageBlob: Blob) => {
+					// imageBlob is the binary data of the the image
+					// From here you can manipulate it and store it where you want
+					// For example, to store it in your app dir
+					// The replace true is optional but is just in case you want to overwrite it
+					return this.file.writeFile(this.file.dataDirectory, "my_downloaded_image", imageBlob, {replace: true});
+			}).toPromise();
+	}
+	*/
 
+	private loadclose() {
+		setTimeout(() => {
+			//this.postsLoading = '1';
+		}, 1000);
+	}
 
-/*
-	  
-	  setTimeout(() => {
-			  fetch('https://www.radiolac.ch/wp-json/mog/v1/get_data?clean=true&post_id='+navParams.get('key'))
-				.then(response => response.json())
-				.then(data => {
-				  console.log(data);
-				  for(let i of data){
-						//this.posts(i);
-					  this.title = i.title;
-					  this.image = i.card;
-					  this.text = sanitizer.bypassSecurityTrustHtml(i.content);
-					  
-					 
-					  
-					}
+	private resize() {
+		var iframe = $('#remotedata');
+		$('#remotedata').height(iframe[0].contentWindow.document.body.scrollHeight + 400);
+		//alert('ok');
+	}
 
-
-				});
-			},20);
-	*/  
-	this.trustedPostUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.link);
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad DetailsPage');
+		//this.trustedPostUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.link+'?clean=true');
 
 
 	}
 
-	openLocalPdf() {
-    
-  }
- 
-  downloadAndOpenPdf() {
-    let path = null;
- 
-    if (this.plt.is('ios')) {
-      path = this.file.documentsDirectory;
-    } else if (this.plt.is('android')) {
-      path = this.file.dataDirectory;
-    }
- 
-    const transfer = this.transfer.create();
-    transfer.download('https://devdactic.com/html/5-simple-hacks-LBT.pdf', path + 'myfile.pdf').then(entry => {
-      let url = entry.toURL();
-     // this.document.viewDocument(url, 'application/pdf', {});
-    });
-  }
-
-/*
-private downloadAndSavePicture(pictureUrl) {
-    this.http.get(pictureUrl, {responseType: 'blob'})
-    .subscribe((imageBlob: Blob) => {
-        // imageBlob is the binary data of the the image
-        // From here you can manipulate it and store it where you want
-        // For example, to store it in your app dir
-        // The replace true is optional but is just in case you want to overwrite it
-        return this.file.writeFile(this.file.dataDirectory, "my_downloaded_image", imageBlob, {replace: true});
-    }).toPromise();
-}
-*/
-	
-private loadclose(){
-			setTimeout( () => {
-				//this.postsLoading = '1';
-			}, 1000 );
-}	
-	
-private resize(){
-	var iframe = $('#remotedata');
-	$('#remotedata').height(iframe[0].contentWindow.document.body.scrollHeight + 40);
-	//alert('ok');
-}	
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DetailsPage');
-	//this.trustedPostUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.link+'?clean=true');
-
-
-  }
-	
 	private dismiss() {
 		this.viewCtrl.dismiss();
 	}
 
-	
-	private share(){
-    this.socialSharing.share(this.navParams.get('text'), this.navParams.get('title'), null, this.navParams.get('link'))
-      .then(()=>{
-       //
-      },
-      ()=>{
-         //
-      })
-  }
-	
+
+	private share() {
+		this.socialSharing.share(this.navParams.get('text'), this.navParams.get('title'), null, this.navParams.get('link'))
+			.then(() => {
+				//
+			},
+				() => {
+					//
+				})
+	}
+
 }
